6882a5fe4f6660e6525f0ac0e4cb6cc48776fd4f
hcunytcc <hcuny.tcc@gmail.com>
2018-06-11 09:51:07 -1000

    instruction + boutton filtre ajouté

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,84 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  testCheckboxOpen: boolean;
+  testCheckboxResult;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController ) {
 
   }
+  
+  doCheckbox() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Which planets have you visited?');
 
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Alderaan',
+      value: 'value1',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Bespin',
+      value: 'value2'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Coruscant',
+      value: 'value3'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Endor',
+      value: 'value4'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Hoth',
+      value: 'value5'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Jakku',
+      value: 'value6'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Naboo',
+      value: 'value6'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Takodana',
+      value: 'value6'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Tatooine',
+      value: 'value6'
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'Okay',
+      handler: data => {
+        console.log('Checkbox data:', data);
+        this.testCheckboxOpen = false;
+        this.testCheckboxResult = data;
+      }
+    });
+    alert.present().then(() => {
+      this.testCheckboxOpen = true;
+    });
+  }
 }
+
